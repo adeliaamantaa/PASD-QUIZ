41d0246d7c9a7a58af63828d16dc811b4e1f7ea8
Adeliaa <140463906+adeliaamantaa@users.noreply.github.com>
2026-03-07 10:19:46 +0700

    Update DosenDemo_01.java

--- DosenDemo_01.java
+++ DosenDemo_01.java
@@ -26,24 +26,36 @@ public class DosenDemo_01 {
         System.out.println("--------------------------");
         dosen.tampilkanInfo();
 
-        System.out.println("\nPilih data yang ingin diubah");
-        System.out.println("1. Ubah Prodi");
-        System.out.println("2. Ubah Jenjang Pendidikan");
-        System.out.print("Pilihan: ");
-        int pilih = sc.nextInt();
-        sc.nextLine();
+        int pilihan;
 
-        if(pilih == 1){
-            System.out.print("Masukkan Prodi Baru: ");
-            String prodiBaru = sc.nextLine();
-            dosen.ubahProdi(prodiBaru);
-        } else if(pilih == 2){
-            System.out.print("Masukkan Jenjang Pendidikan Baru: ");
-            String pendidikanBaru = sc.nextLine();
-            dosen.ubahJPendidikan(pendidikanBaru);
-        } else {
-            System.out.println("Pilihan tidak tersedia");
-        }
+        do {
+            System.out.println("\n===== MENU UBAH DATA =====");
+            System.out.println("1. Ubah Prodi");
+            System.out.println("2. Ubah Jenjang Pendidikan");
+            System.out.println("3. Keluar");
+            System.out.print("Pilih menu: ");
+            pilihan = sc.nextInt();
+            sc.nextLine();
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan Prodi Baru: ");
+                    String prodiBaru = sc.nextLine();
+                    dosen.ubahProdi(prodiBaru);
+                    System.out.println("Prodi berhasil diubah!");
+                    break;
+                case 2:
+                    System.out.print("Masukkan Jenjang Pendidikan Baru: ");
+                    String pendidikanBaru = sc.nextLine();
+                    dosen.ubahJPendidikan(pendidikanBaru);
+                    System.out.println("Jenjang pendidikan berhasil diubah!");
+                    break;
+                case 3:
+                    System.out.println("Program selesai.");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak tersedia!");
+            }
+        } while (pilihan != 3);
 
         System.out.println("--------------------------");
         System.out.println("\tDATA BARU");
