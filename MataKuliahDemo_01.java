8ac30dbf0880544ff87dba0769b4602b8bae341a
Adeliaa <140463906+adeliaamantaa@users.noreply.github.com>
2026-03-07 09:50:42 +0700

    Update MataKuliahDemo_01.java

--- MataKuliahDemo_01.java
+++ MataKuliahDemo_01.java
@@ -1,24 +1,73 @@
+import java.util.Scanner;
+
 public class MataKuliahDemo_01 {
 
     public static void main(String[] args) {
 
-        System.out.println("----------------------------");
-        System.out.println("\tTAMPIL DATA");
-        System.out.println("----------------------------");
-        Dosen_01 ds1 = new Dosen_01("1234567890","Pak Dosen",2010,"S2","TI");
-        Dosen_01 ds2 = new Dosen_01("0987654321","Bu Dosen",2012,"S3","TI");
+        Scanner sc = new Scanner(System.in);
 
-        MataKuliah_01 mk1 = new MataKuliah_01("1234567890","Algoritma dan Struktur Data",3,3);
+        System.out.println("----------------------------");
+        System.out.println("\tINPUT DATA DOSEN");
+        System.out.println("----------------------------");
+        System.out.print("NIDN Dosen 1: ");
+        String nidn1 = sc.nextLine();
+        System.out.print("Nama Dosen 1: ");
+        String nama1 = sc.nextLine();
+        System.out.print("Tahun Masuk: ");
+        int tahun1 = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Jenjang Pendidikan: ");
+        String jp1 = sc.nextLine();
+        System.out.print("Prodi: ");
+        String prodi1 = sc.nextLine();
+
+        Dosen_01 ds1 = new Dosen_01(nidn1,nama1,tahun1,jp1,prodi1);
+
+        System.out.println();
+        System.out.print("NIDN Dosen 2: ");
+        String nidn2 = sc.nextLine();
+        System.out.print("Nama Dosen 2: ");
+        String nama2 = sc.nextLine();
+        System.out.print("Tahun Masuk: ");
+        int tahun2 = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Jenjang Pendidikan: ");
+        String jp2 = sc.nextLine();
+        System.out.print("Prodi: ");
+        String prodi2 = sc.nextLine();
+
+        Dosen_01 ds2 = new Dosen_01(nidn2,nama2,tahun2,jp2,prodi2);
+
+        System.out.println("----------------------------");
+        System.out.println("\tINPUT DATA MATAKULIAH");
+        System.out.println("----------------------------");
+        System.out.print("Kode MK: ");
+        String kode = sc.nextLine();
+        System.out.print("Nama MK: ");
+        String namaMK = sc.nextLine();
+        System.out.print("SKS: ");
+        int sks = sc.nextInt();
+
+        MataKuliah_01 mk1 = new MataKuliah_01(kode,namaMK,sks,3);
 
         mk1.tambahDosen(ds1);
         mk1.tambahDosen(ds2);
 
+        System.out.println("----------------------------");
+        System.out.println("\tTAMPIL DATA");
+        System.out.println("----------------------------");
         mk1.tampilkanInfo();
+        
+        sc.nextLine();
+        
+        System.out.print("Ubah Nama MK: ");
+        String namaBaru = sc.nextLine();
+        mk1.ubahNamaMK(namaBaru);
+        System.out.print("Ubah SKS: ");
+        int sksBaru = sc.nextInt();
+        mk1.ubahSKS(sksBaru);
 
-        mk1.ubahNamaMK("Struktur Data");
-        mk1.ubahSKS(4);
-
-        System.out.println("\t DATA BARU");
+        System.out.println("\tDATA BARU");
         System.out.println("----------------------------");
         mk1.tampilkanInfo();
     }
