b751b6539294b479e70d50d1d353b3270d621a65
Adeliaa <140463906+adeliaamantaa@users.noreply.github.com>
2026-03-07 10:12:06 +0700

    Update DosenDemo_01.java

--- DosenDemo_01.java
+++ DosenDemo_01.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
 public class DosenDemo_01 {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         System.out.println("--------------------------");
@@ -22,19 +20,30 @@ public class DosenDemo_01 {
         String prodi = sc.nextLine();
 
         Dosen_01 dosen = new Dosen_01(nidn, nama, tahunMasuk, pendidikan, prodi);
-        
+
         System.out.println("--------------------------");
         System.out.println("\tTAMPIL DATA");
         System.out.println("--------------------------");
         dosen.tampilkanInfo();
-        
-        System.out.println("\nUbah Data Dosen");
-        System.out.print("Masukkan Prodi Baru: ");
-        String prodiBaru = sc.nextLine();
-        dosen.ubahProdi(prodiBaru);
-        System.out.print("Masukkan Jenjang Pendidikan Baru: ");
-        String pendidikanBaru = sc.nextLine();
-        dosen.ubahJPendidikan(pendidikanBaru);
+
+        System.out.println("\nPilih data yang ingin diubah");
+        System.out.println("1. Ubah Prodi");
+        System.out.println("2. Ubah Jenjang Pendidikan");
+        System.out.print("Pilihan: ");
+        int pilih = sc.nextInt();
+        sc.nextLine();
+
+        if(pilih == 1){
+            System.out.print("Masukkan Prodi Baru: ");
+            String prodiBaru = sc.nextLine();
+            dosen.ubahProdi(prodiBaru);
+        } else if(pilih == 2){
+            System.out.print("Masukkan Jenjang Pendidikan Baru: ");
+            String pendidikanBaru = sc.nextLine();
+            dosen.ubahJPendidikan(pendidikanBaru);
+        } else {
+            System.out.println("Pilihan tidak tersedia");
+        }
 
         System.out.println("--------------------------");
         System.out.println("\tDATA BARU");
